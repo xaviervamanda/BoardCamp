273a024e27a324b10505136b145c19e418c25d99
Amanda Xavier <amandavxavier@gmail.com>
2023-05-11 09:44:03 -0300

    Feat: function createGame

--- src/controllers/game.controllers.js
+++ src/controllers/game.controllers.js
@@ -2,7 +2,7 @@ import {db} from "../database/database.connection.js";
 
 export async function listGames(req, res){
     try{
-        const games = await db.query("SELECT * FROM games");
+        const games = await db.query("SELECT * FROM games")
         return res.status(200).send(games.rows);
     } catch (err) {
         return res.status(500).send(err.message);
@@ -10,5 +10,17 @@ export async function listGames(req, res){
 }
 
 export async function createGame(req, res){
+    const {name, image, stockTotal, pricePerDay} = req.body;
 
+    try{
+        const gameName = await db.query("SELECT * FROM games WHERE name = $1", [name]);
+        if (gameName.rowCount !== 0){
+            return res.status(409).send("Game already exists");
+        }
+        await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4)`, 
+        [name, image, stockTotal, pricePerDay]);
+        return res.sendStatus(201);
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
 }
