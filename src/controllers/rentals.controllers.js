49c4319883bc3df86000ea638fe0dfe5adca5977
Amanda Xavier <amandavxavier@gmail.com>
2023-05-12 14:11:25 -0300

    Feat: function deleteRental

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -45,3 +45,17 @@ export async function createRental(req, res){
         return res.status(500).send(err.message);
     }
 }
+
+export async function deleteRental(req, res) {
+    const {id} = req.params;
+
+    try{
+        const rental = await db.query(`SELECT * FROM rentals WHERE "id" = $1;`, [id]);
+        if (rental.rowCount === 0) return res.status(404).send("Rental not found");
+        if (rental.rows[0].returnDate === null) return res.status(400).send("Rental not returned yet, it can't be deleted");
+        await db.query(`DELETE FROM rentals WHERE "id" = $1;`, [id]);
+        return res.sendStatus(200);
+    } catch (err){
+        return res.status(500).send(err.message);
+    }
+}
--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/schemaValidation.js";
 import {rentalSchema} from "../schemas/rentals.schemas.js";
-import { listRentals, createRental } from "../controllers/rentals.controllers.js";
+import { listRentals, createRental, deleteRental } from "../controllers/rentals.controllers.js";
 
 const rentalsRouter = Router();
 
 rentalsRouter.get("/rentals", listRentals);
 rentalsRouter.post("/rentals", validateSchema(rentalSchema), createRental);
 // rentalsRouter.post("/rentals/:id/return", returnRental);
-// rentalsRouter.delete("/rentals/:id", deleteRental);
+rentalsRouter.delete("/rentals/:id", deleteRental);
 
 export default rentalsRouter;
