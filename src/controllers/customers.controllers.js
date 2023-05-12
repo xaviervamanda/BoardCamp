4ac619853aed86d2736ceec08907b78a84503e35
Amanda Xavier <amandavxavier@gmail.com>
2023-05-12 13:13:49 -0300

    Feat: function getCustomerById

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -25,3 +25,16 @@ export async function listCustomers(req, res){
         return res.status(500).send(err.message);
     }
 }
+
+export async function getCustomerById(req, res){
+    const {id} = req.params;
+    try{
+        const customer = await db.query("SELECT id, name, phone, cpf, to_char(birthday, 'YYYY-MM-DD') as birthday FROM customers WHERE id = $1;", [id]);
+        if (customer.rowCount === 0){
+            return res.status(404).send("Customer not found");
+        }
+        return res.status(200).send(customer.rows[0]);
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
+}
--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/schemaValidation.js";
 import {customerSchema} from "../schemas/customers.schemas.js";
-import { addCustomer, listCustomers } from "../controllers/customers.controllers.js";
+import { addCustomer, listCustomers, getCustomerById } from "../controllers/customers.controllers.js";
 
 const customerRouter = Router();
 
 customerRouter.get("/customers", listCustomers);
-// customerRouter.get("/customers/:id", getCustomerById);
+customerRouter.get("/customers/:id", getCustomerById);
 customerRouter.post("/customers", validateSchema(customerSchema), addCustomer);
 // customerRouter.put("/customers/:id", validateSchema(customerSchema), updateCustomer);
 
