c51333f6e5b609d7dd4b870d42c21a18c9ec9f45
Amanda Xavier <amandavxavier@gmail.com>
2023-05-13 14:41:37 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -10,5 +10,5 @@ app.use(router);
 dotenv.config();
 
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
