c85ebe25dd33ff342ba9ba449817330a001d8c93
Amanda Xavier <amandavxavier@gmail.com>
2023-05-13 10:23:05 -0300

    Fix: function updateCustomer

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -4,11 +4,11 @@ export async function addCustomer(req, res) {
     const {name, phone, cpf, birthday} = req.body;
 
     try{
-        const customerCpf = await db.query("SELECT * FROM customers WHERE cpf = $1", [cpf]);
+        const customerCpf = await db.query("SELECT * FROM customers WHERE cpf = $1;", [cpf]);
         if (customerCpf.rowCount !== 0){
             return res.status(409).send("CPF already registered");
         }
-        await db.query("INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4)",
+        await db.query("INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4);",
          [name, phone, cpf, birthday]);
         return res.sendStatus(201);
     } catch (err) {
@@ -44,16 +44,17 @@ export async function updateCustomer(req, res){
     const {name, phone, cpf, birthday} = req.body;
 
     try{
-        const customerCpf = await db.query("SELECT * FROM customers WHERE cpf = $1", [cpf]);
+        const customer = await db.query("SELECT * FROM customers WHERE id = $1;", [id]);
+        if (customer.rowCount === 0) return res.status(404).send("Customer not found");
+        const customerCpf = await db.query("SELECT * FROM customers WHERE cpf = $1;", [cpf]);
         if (customerCpf.rowCount !== 0){
             if (customerCpf.rows[0].id !== id){
                 return res.status(409).send("CPF already registered");
             }
-            await db.query(`UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5`,
-            [name, phone, cpf, birthday, id]);
-            return res.sendStatus(200);
         }
-        
+        await db.query(`UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5;`,
+        [name, phone, cpf, birthday, id]);
+        return res.sendStatus(200);
     } catch (err){
         return res.status(500).send(err.message);
     }
